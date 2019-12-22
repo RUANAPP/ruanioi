dce9c789ba6b9540190fa05d8b50bc61450782d7
ssyz01 <ssyz_01@outlook.com>
2019-12-22 16:56:14 +0800

    p09103

--- p09103.cpp
+++ p09103.cpp
@@ -4,7 +4,7 @@ inline int fun(int x){
     if(x==0||x==1)return 3;
     else return x-fun(x-2);
 }
-int main(int argc, char const *argv[])
+    int main(int argc, char const *argv[])
 {
     printf("%d",fun(9));
     putchar('\n');
