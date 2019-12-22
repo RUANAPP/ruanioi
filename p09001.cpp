7a72e2b428e8f2e087af1964ba91115342ad5fc6
ssyz01 <ssyz_01@outlook.com>
2019-12-22 16:38:44 +0800

    int 内置

--- p09001.cpp
+++ p09001.cpp
@@ -6,8 +6,7 @@ int s,maxx;
 int a[11];
 int main(int argc, char const *argv[])
 {
-    int i;
-    for(i=1;i<=10;++i)cin>>a[i];
+    for(int i=1;i<=10;++i)cin>>a[i];
     maxx=a[1];s=a[1];
     for(int i=2;i<=10;++i){
         if(s<0)s=0;
