dc4147e7a9bef3c2f1ba85e024a9dd0b41cda803
ssyz01 <ssyz_01@outlook.com>
2019-12-29 16:54:20 +0800

    p090001~p091004

---
+++ p090001.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+int s, maxx;
+int a[11];
+int main()
+{
+    for (int i = 1; i <= 10; ++i)
+        cin >> a[i];
+    maxx = a[1];
+    s = a[1];
+    for (int i = 2; i <= 10; ++i)
+    {
+        if (s < 0)
+            s = 0;
+        s = s + a[i];
+        if (s > maxx)
+            maxx = s;
+    }
+    printf("%d", maxx);
+}
