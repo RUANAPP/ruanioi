81c83a2c9ac4587b03e4ee2dde176ee42ed1fa95
ssyz01 <ssyz_01@outlook.com>
2019-12-29 16:13:35 +0800

    (2019/12/29)

--- p09001.cpp
+++ p09001.cpp
@@ -4,19 +4,25 @@
 using namespace std;
 int s, maxx;
 int a[11];
-int main(){
-    for(int i=1;i<=10;++i){
-        cin>>a[i];
-        maxx=a[1];s=a[1];
+int main()
+{
+    for (int i = 1; i <= 10; ++i)
+    {
+        cin >> a[i];
+        maxx = a[1];
+        s = a[1];
     }
-    for(int i=2;i<=10;++i){
-        if(s<0){
-            s=0;
-            s=s+a[i];
+    for (int i = 2; i <= 10; ++i)
+    {
+        if (s < 0)
+        {
+            s = 0;
+            s = s + a[i];
         }
-        if(s>maxx){
-            maxx=s;
+        if (s > maxx)
+        {
+            maxx = s;
         }
     }
-    printf("%d",maxx);
+    printf("%d", maxx);
 }
