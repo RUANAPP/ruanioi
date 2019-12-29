01843f1a55a1b488c03c766a146113d1d499d32b
ssyz01 <ssyz_01@outlook.com>
2019-12-29 16:54:16 +0800

    hhhhhhhhhhhhhhhhhhhhhh

---
+++ p092005.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+using namespace std;
+int i,j,h,m,n,k,b[11];
+int main(int argc, char const *argv[])
+{
+    scanf("%d",&n);
+    for(i=1;i<=10;i++){
+    m=n;j=11;
+    while(m>0){
+        j=j-1;b[j]=m%10;m=m/10;
+    }
+    for(h=1;h<=10;h++)n=n+b[h];
+}
+printf("%d",n);
+}
