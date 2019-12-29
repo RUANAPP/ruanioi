8ced99560bc5a45040985e7c7c7db5fda10ef253
ssyz01 <ssyz_01@outlook.com>
2019-12-29 15:11:13 +0800

    I Am Happy? today? (:)

---
+++ sapus.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main(){
+    char M;
+    scanf("%c",&M);
+    cout<<"  "<<M<<endl;
+    cout<<" "<<M<<M<<M<<endl;
+    cout<<M<<M<<M<<M<<M<<endl;
+    cout<<" "<<M<<M<<M<<endl;
+    cout<<"  "<<M<<endl;
+    cout<<"__________";
+    return 0;
+}
