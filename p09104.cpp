8c3b029d998ed1e4f0fd06c4ccf04f78f2d1b734
ssyz01 <ssyz_01@outlook.com>
2019-12-29 15:47:42 +0800

    yes

---
+++ p09104.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+using namespace std;
+int ack(int m,int n){
+    if(m==0)return n+1;
+    else if(n==0)return ack(m-1,1);
+    else return ack(m-1,ack(m,n-1));
+}
+int main(int argc, char const *argv[])
+{
+    printf("%d\n",ack(3,4));
+    putchar('\n');
+    return 0;
+}
