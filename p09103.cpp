86cf20d6a8360ad76799bf1eeb18caaa2bc208f6
ssyz01 <ssyz_01@outlook.com>
2019-12-29 16:22:23 +0800

    p09103,p09104

---
+++ p09103.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+using namespace std;
+inline int fun(int x)
+{
+    if(x==0||x==1){
+    return 3;
+    }
+    else{
+        return x- fun(x-2);
+    }
+}
+int main(int argc, char const *argv[])
+{
+    printf("%d",fun(9));
+    putchar('\n');
+    return 0;
+}
